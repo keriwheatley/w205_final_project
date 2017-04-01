a2744a6eadd13fb3e7e3a5580e2b85982b99395f
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-01 11:35:07 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -6,8 +6,8 @@ import psycopg2
 def data_extract():
     try:
         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
-    sql = "INSERT INTO issued_construction_permits VALUES (1,%s);" %(row)
-    cur.execute(sql);
+        sql = "INSERT INTO issued_construction_permits VALUES (1,%s);" %(row)
+        cur.execute(sql);
     except Exception as inst:
         print(inst.args)
         print(inst)
