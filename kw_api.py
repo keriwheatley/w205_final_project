aa7f2f58b18d4f70048818613459bc7945c44d19
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-02 11:05:05 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -18,25 +18,25 @@ def create_table():
 
 create_table()
 
-# def data_extract():
-#     try:
-#         url = 'https://data.austintexas.gov/resource/x9yh-78fz.json?permittype=EP'
-#         response = requests.get(url, verify=False)
-#         if response.status_code == 200:
-#             data = response.json()
-#         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
-#         cur = conn.cursor()
+def data_extract():
+    try:
+        url = 'https://data.austintexas.gov/resource/x9yh-78fz.json?permittype=EP'
+        response = requests.get(url, verify=False)
+        if response.status_code == 200:
+            data = response.json()
+        conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
+        cur = conn.cursor()
 
-#         for row in data:
-#             sql = 'INSERT INTO issued_construction_permits VALUES ('
-#             for i in row:
-#                 sql += "'" + str(row[i]).replace("'","''") + "',"
-#             sql = sql[:-1] + ');'
-#             print sql
-#             cur.execute(sql);
-#     except Exception as inst:
-#         print(inst.args)
-#         print(inst)
+        for row in data:
+            sql = 'INSERT INTO issued_construction_permits VALUES ('
+            for i in row:
+                sql += "'" + str(row[i]).replace("'","''") + "',"
+            sql = sql[:-1] + ');'
+            print sql
+            cur.execute(sql);
+    except Exception as inst:
+        print(inst.args)
+        print(inst)
 
 # data_extract()
 
