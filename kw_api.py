4fb6a1739c62d305f7abfc65bc0b5efa63d37aab
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-02 11:06:39 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -30,6 +30,7 @@ def data_extract():
         for row in data:
             sql = 'INSERT INTO issued_construction_permits VALUES ('
             for i in row:
+                print i + ' ' + row(i)
                 sql += "'" + str(row[i]).replace("'","''") + "',"
             sql = sql[:-1] + ');'
             print sql
