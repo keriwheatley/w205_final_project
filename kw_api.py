f33e545d109cf617c6b959f6489ddcd568103f40
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-02 11:01:46 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -3,27 +3,35 @@ import datetime
 import json
 import psycopg2
 
-def data_extract():
-    try:
-        url = 'https://data.austintexas.gov/resource/x9yh-78fz.json?permittype=EP'
-        response = requests.get(url, verify=False)
-        if response.status_code == 200:
-            data = response.json()
-        conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
-        cur = conn.cursor()
-
-        for row in data:
-            sql = 'INSERT INTO issued_construction_permits VALUES ('
+def create_table():
+    for row in data:
+        sql = 'CREATE TABLE issued_construction_permits ('
             for i in row:
-                print i
-                print row[i]
-                sql += "'" + str(row[i]).replace("'","''") + "',"
-            sql = sql[:-1] + ');'
-            print sql
-            cur.execute(sql);
-    except Exception as inst:
-        print(inst.args)
-        print(inst)
+                sql += "'" + str(row[i]).replace("'" text,"''") + "',"
+            sql = sql[:-2] + ');'
+        break
 
-data_extract()
+create_table()
+
+# def data_extract():
+#     try:
+#         url = 'https://data.austintexas.gov/resource/x9yh-78fz.json?permittype=EP'
+#         response = requests.get(url, verify=False)
+#         if response.status_code == 200:
+#             data = response.json()
+#         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
+#         cur = conn.cursor()
+
+#         for row in data:
+#             sql = 'INSERT INTO issued_construction_permits VALUES ('
+#             for i in row:
+#                 sql += "'" + str(row[i]).replace("'","''") + "',"
+#             sql = sql[:-1] + ');'
+#             print sql
+#             cur.execute(sql);
+#     except Exception as inst:
+#         print(inst.args)
+#         print(inst)
+
+# data_extract()
 
