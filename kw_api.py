ab1df9246d4678c808995c0a82b14700e6c03ef9
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-03 17:01:38 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -21,7 +21,7 @@ import psycopg2
 
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+        yield start_date + datetime.timedelta(n)
 
 def data_extract():
     try:
