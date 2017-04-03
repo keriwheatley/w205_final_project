474556547b942fcad39f6d15fabf323732e0603e
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-03 17:55:04 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -37,7 +37,7 @@ def data_extract():
         
         print start_date
         print type(start_date)
-#         for single_date in daterange(start_date, current_day-1):
+        for single_date in daterange(start_date, current_day-1):
 #             applied_date=str(single_date.strftime("%Y-%m-%d"))
 #             url = "https://data.austintexas.gov/resource/x9yh-78fz.json?$limit=50000&applieddate="+applied_date
 #             print url
@@ -63,7 +63,7 @@ def data_extract():
 #                 sql = 'INSERT INTO issued_construction_permits (' + columns + ') VALUES (' + values + ');'
 #                 cur.execute(sql);
 #             cur.execute("INSERT INTO issued_construction_permits_counts VALUES('"+applied_date+"',"+str(num_rows)+");")
-#             conn.commit()
+            conn.commit()
 
 #         conn.close()
         
