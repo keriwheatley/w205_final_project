de3224bb9de685f867e0c70911f641788b40e210
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-04 07:25:39 -0500

    Update kw_api.py

--- kw_api.py
+++ kw_api.py
@@ -32,8 +32,9 @@ def data_extract():
 #         end_date = datetime.date.today() - datetime.timedelta(days=1)
         cur.execute("SELECT MAX(applied_date) FROM issued_construction_permits_counts;");
         last_run = cur.fetchall()
-        if last_run is None: start_date = datetime.date(1990, 1, 1)
-        else: start_date = datetime.date(last_run[0][0])
+        print last_run
+#         if last_run is None: start_date = datetime.date(1990, 1, 1)
+#         else: start_date = datetime.date(last_run[0][0])
 #         print end_date
 #         print type(end_date)
 #         print start_date
