9bc21a5e4f923035653f5872ec1d242445028c78
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-05 11:02:34 -0500

    Update austin_open_data_api.py

--- api/austin_open_data_api.py
+++ api/austin_open_data_api.py
@@ -62,7 +62,7 @@ def data_extract(data_source, initial_start_date, date_format, api_url):
 
             # Commit changes to tables for single date
             conn.commit()
-            print "Loaded " + single_date + " records."
+            print "Loaded " + single_date + " records to data source ("+table_name+")."
 
         # Close connection after all single dates have been processed
         conn.close()
