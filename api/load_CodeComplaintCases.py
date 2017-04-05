ff967971a81ede14ff815185ecb88f4ce6631cbb
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-05 11:21:34 -0500

    Update load_CodeComplaintCases.py

--- api/load_CodeComplaintCases.py
+++ api/load_CodeComplaintCases.py
@@ -6,4 +6,4 @@ initial_start_date = datetime.date(1990, 1, 1) # Start date for initial load
 end_date = datetime.date.today()-datetime.timedelta(days=1) # End time is yesterday
 date_format = "strftime('%Y-%m-%d')" # Format date for API call
 api_url = "https://data.austintexas.gov/resource/cgku-nb4s.json?$limit=50000&opened_date="
-data_extract(table_name,initial_start_date,end_date,date_format,api_url)
+load_data_code.data_extract(table_name,initial_start_date,end_date,date_format,api_url)
