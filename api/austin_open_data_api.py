ef41118dd6bf5b47d87d9d0b032c808190f6ed3a
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-05 11:01:15 -0500

    Update austin_open_data_api.py

--- api/austin_open_data_api.py
+++ api/austin_open_data_api.py
@@ -73,12 +73,12 @@ def data_extract(data_source, initial_start_date, date_format, api_url):
         print(inst.args)
         print(inst)
 
-table_name = "issued_construction_permits"
-initial_start_date = datetime.date(1990, 1, 1) # Start date for initial load; initial load runtime ~30 minutes
-end_date = datetime.date.today()-datetime.timedelta(days=1) # End time is yesterday
-date_format = "strftime('%Y-%m-%d')" # Format date for API call
-api_url = "https://data.austintexas.gov/resource/x9yh-78fz.json?$limit=50000&applieddate="
-data_extract(table_name,initial_start_date,date_format,api_url)
+# table_name = "issued_construction_permits"
+# initial_start_date = datetime.date(1990, 1, 1) # Start date for initial load; initial load runtime ~30 minutes
+# end_date = datetime.date.today()-datetime.timedelta(days=1) # End time is yesterday
+# date_format = "strftime('%Y-%m-%d')" # Format date for API call
+# api_url = "https://data.austintexas.gov/resource/x9yh-78fz.json?$limit=50000&applieddate="
+# data_extract(table_name,initial_start_date,date_format,api_url)
 
 table_name = "restaurant_inspection_scores"
 initial_start_date = datetime.datetime(2014, 3, 1,hour=19) # Start date for initial load; initial load runtime ~1 minute
