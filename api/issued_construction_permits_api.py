23f85829759a8e2d77348ab49bce1cb998b2b845
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-05 11:16:16 -0500

    Update issued_construction_permits_api.py

--- api/issued_construction_permits_api.py
+++ api/issued_construction_permits_api.py
@@ -6,4 +6,4 @@ initial_start_date = datetime.date(1990, 1, 1) # Start date for initial load; in
 end_date = datetime.date.today()-datetime.timedelta(days=1) # End time is yesterday
 date_format = "strftime('%Y-%m-%d')" # Format date for API call
 api_url = "https://data.austintexas.gov/resource/x9yh-78fz.json?$limit=50000&applieddate="
-data_extract.data_extract(table_name,initial_start_date,date_format,api_url)
+data_extract.data_extract(table_name,initial_start_date,end_date,date_format,api_url)
