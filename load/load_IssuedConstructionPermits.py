07decbd9127bc8cf204e5689975b86e6a94f036d
Keri Wheatley <keri.wheatley@civitaslearning.com>
2017-04-05 20:04:03 -0500

    change folder name

---
+++ load/load_IssuedConstructionPermits.py
@@ -0,0 +1,71 @@
+import requests
+import datetime
+import json
+import psycopg2
+
+def data_extract():
+    try:        
+        # Start runtime
+        start_time = datetime.datetime.now()
+        print "Starting data extract for data source (issued_construction_permits) at time (" + str(start_time) + ")."
+        
+        # Connect to database
+        conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
+        cur = conn.cursor()
+                
+        # Empty data tables
+        cur.execute("TRUNCATE TABLE issued_construction_permits;");
+        cur.execute("TRUNCATE TABLE issued_construction_permits_counts;");
+        print "Truncated data tables."
+        
+        # Iterate through all zip codes and years
+        for year in range(1990,datetime.date.today().year):
+            for zip in ['78610', '78613', '78617', '78641', '78652', '78653', '78660', '78664', '78681', '78701', '78702', 
+                '78703', '78704', '78705', '78712', '78717', '78719', '78721', '78722', '78723', '78724', '78725',
+                '78726', '78727', '78728', '78729', '78730', '78731', '78732', '78733', '78734', '78735', '78736',
+                '78737', '78738', '78739', '78741', '78742', '78744', '78745', '78746', '78747', '78748', '78749',
+                '78750', '78751', '78752', '78753', '78754', '78756', '78757', '78758', '78759']:
+            
+                # Make API call to data source
+                url = "https://data.austintexas.gov/resource/x9yh-78fz.json?$limit=50000&original_zip="+zip+"&calendar_year_issued="+str(year)
+                response = requests.get(url, verify=False)
+                data = response.json()
+                if response.status_code <> 200:
+                    print "Error: Did not complete call to API. Check API call: " + url
+                    print data
+                    break
+
+                # Print row count for single year/zip code
+                num_rows = len(data)
+                row_format = "{:>20}" *(8)
+                print row_format.format('Zip_Code:', zip,'Year:', year, 'Row_Count:',str(num_rows),
+                    'Runtime:',str((datetime.datetime.now() - start_time)))
+
+                # Write each row for single date to data lake table
+                for row in data:
+                    values = ""
+                    columns = ""
+                    for i in row:
+                        columns += str(i) + ","                
+                        values += "'" + str(row[i]).replace("'","") + "',"
+                    columns = columns[:-1]
+                    values = values[:-1]
+                    cur.execute("INSERT INTO issued_construction_permits (" + columns + ") VALUES (" + values + ");");
+
+                # Record row count for single year/zip code to counts table
+                cur.execute("INSERT INTO issued_construction_permits_counts VALUES("+zip+","+str(year)+","+str(num_rows)+");")
+
+                # Commit changes to tables for single year/zip code
+                conn.commit()
+                print "Loaded records to data source (issued_construction_permits) for zip code ("+zip+") and year ("+str(year)+")."
+
+        # Close connection after all single dates have been processed
+        conn.close()
+        print "Ended data extract for data source (issued_construction_permits) at time (" + str(datetime.datetime.now()) + ")."
+    
+    # Error logging
+    except Exception as inst:
+        print(inst.args)
+        print(inst)
+            
+data_extract()
---
+++ load/load_RacialProfilingCitations.py
@@ -0,0 +1,79 @@
+import requests
+import datetime
+import json
+import psycopg2
+
+def data_extract():
+    try:        
+        # Start runtime
+        start_time = datetime.datetime.now()
+        print "Starting data extract for data source (racial_profiling_citations) at time (" + str(start_time) + ")."
+        
+        # Connect to database
+        conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
+        cur = conn.cursor()
+
+        # Empty data tables
+        cur.execute("TRUNCATE TABLE racial_profiling_citations;");
+        cur.execute("TRUNCATE TABLE racial_profiling_citations_counts;");
+        print "Truncated data tables."
+
+        # 2014 data API link
+        api_2014 = "https://data.austintexas.gov/resource/rkqq-yay6.json?$limit=50000&race_origin_code="
+        # 2015 data API link
+        api_2015 = "https://data.austintexas.gov/resource/5tcj-brxc.json?$limit=50000&race_origin_code="            
+        # 2016 data API link
+        api_2016 = "https://data.austintexas.gov/resource/gmmi-p5zw.json?$limit=50000&race_origin_code="
+        
+        api_links=[api_2014, api_2015, api_2016]
+        years=['2014','2015','2016']
+
+        # Iterate through all years and races
+        for year_index in xrange(2):
+            for race in ['A','B','H','ME','N','O','W']:
+                # Make API call to data source
+                url = api_links[year_index]+race
+                response = requests.get(url, verify=False)
+                data = response.json()
+                if response.status_code <> 200:
+                    print "Error: Did not complete call to API. Check API call: " + url
+                    print data
+                    break
+
+                # Print row count for single year/race
+                num_rows = len(data)
+                row_format = "{:>20}" *(8)
+                print row_format.format('Year:', years[year_index], 'Race:', race, 'Row_Count:',str(num_rows),
+                    'Runtime:',str((datetime.datetime.now() - start_time)))
+
+                # Write each row for single year/race to data table
+                for row in data:
+                    values = ""
+                    columns = ""
+                    for i in row:
+                        columns += str(i) + ","                
+                        values += "'" + str(row[i]).replace("'","") + "',"
+                    columns = columns[:-1]
+                    values = values[:-1]
+                    cur.execute("INSERT INTO racial_profiling_citations (" + columns + ") VALUES (" + values + ");");
+
+                # Record row count for single year/race to counts table
+                cur.execute("INSERT INTO racial_profiling_citations_counts \
+                    VALUES("+years[year_index]+",'"+race+"',"+str(num_rows)+");")
+
+                # Commit changes to tables for single year/race
+                conn.commit()
+                print "Loaded records to data source (racial_profiling_citations) \
+                    for year ("+years[year_index]+") and race (" +race+"."
+
+        # Close connection after all single year/race have been processed
+        conn.close()
+        print "Ended data extract for data source (racial_profiling_citations) at time ("\
+            + str(datetime.datetime.now()) + ")."
+    
+    # Error logging
+    except Exception as inst:
+        print(inst.args)
+        print(inst)
+            
+data_extract()
