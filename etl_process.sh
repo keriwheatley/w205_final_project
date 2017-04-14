b2f7201aa09af5e14429f4a26b3db1236df2f508
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-14 18:57:12 -0500

    Update etl_process.sh

--- etl_process.sh
+++ etl_process.sh
@@ -1,17 +1,17 @@
 chmod +x ./etl_files/extract_TablesSetup.sh
 ./etl_files/extract_TablesSetup.sh
 
-python ./etl_files/load_CodeComplaintCases.py
-python ./etl_files/load_CommercialWaterConsumption.py
-python ./etl_files/load_IssuedConstructionPermits.py
-python ./etl_files/load_PotholeRepair.py
-python ./etl_files/load_RacialProfilingCitations.py
-python ./etl_files/load_ResidentialWaterConsumption.py
-python ./etl_files/load_RestaurantInspectionScores.py
-python ./etl_files/load_ServiceAlerts.py
+python ./etl_files/extract_CodeComplaintCases.py
+python ./etl_files/extract_CommercialWaterConsumption.py
+python ./etl_files/extract_IssuedConstructionPermits.py
+python ./etl_files/extract_PotholeRepair.py
+python ./etl_files/extract_RacialProfilingCitations.py
+python ./etl_files/extract_ResidentialWaterConsumption.py
+python ./etl_files/extract_RestaurantInspectionScores.py
+python ./etl_files/extract_ServiceAlerts.py
 
 #### Need to load 2014 data. This is in CSV format not API.
-python ./etl_files/load_RacialProfilingArrests.py
+python ./etl_files/extract_RacialProfilingArrests.py
 
 chmod +x ./etl_files/transform_DataAggregates.sh
 ./etl_files/transform_DataAggregates.sh
