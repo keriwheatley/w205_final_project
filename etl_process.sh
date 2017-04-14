4e15b7bd547741742f5706724bd7fa092db9e927
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-14 18:56:12 -0500

    Update etl_process.sh

--- etl_process.sh
+++ etl_process.sh
@@ -1,18 +1,18 @@
 chmod +x ./etl_files/extract_TablesSetup.sh
 ./etl_files/extract_TablesSetup.sh
 
-python etl_files/load_CodeComplaintCases.py
-python etl_files/load_CommercialWaterConsumption.py
-python load_IssuedConstructionPermits.py
-python load_PotholeRepair.py
-python load_RacialProfilingCitations.py
-python load_ResidentialWaterConsumption.py
-python load_RestaurantInspectionScores.py
-python load_ServiceAlerts.py
+python ./etl_files/load_CodeComplaintCases.py
+python ./etl_files/load_CommercialWaterConsumption.py
+python ./etl_files/load_IssuedConstructionPermits.py
+python ./etl_files/load_PotholeRepair.py
+python ./etl_files/load_RacialProfilingCitations.py
+python ./etl_files/load_ResidentialWaterConsumption.py
+python ./etl_files/load_RestaurantInspectionScores.py
+python ./etl_files/load_ServiceAlerts.py
 
 #### Need to load 2014 data. This is in CSV format not API.
-python load_RacialProfilingArrests.py
+python ./etl_files/load_RacialProfilingArrests.py
 
-chmod +x ./transform_DataAggregates.sh
-./transform_DataAggregates.sh
+chmod +x ./etl_files/transform_DataAggregates.sh
+./etl_files/transform_DataAggregates.sh
 
