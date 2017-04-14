109b1b3eaaed88089ab3d9ea1d2b76655a3b748b
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-14 18:42:37 -0500

    Update application_setup.sh

--- application_setup.sh
+++ application_setup.sh
@@ -1,1 +1,19 @@
+cd extract
+
+chmod +x ./extract_tables_setup.sh
+./extract_load_tables_setup.sh
+
+python load_CodeComplaintCases.py
+python load_CommercialWaterConsumption.py
+python load_IssuedConstructionPermits.py
+python load_PotholeRepair.py
+python load_RacialProfilingCitations.py
+python load_ResidentialWaterConsumption.py
+python load_RestaurantInspectionScores.py
+python load_ServiceAlerts.py
+
+#### Need to load 2014 data. This is in CSV format not API.
+python load_RacialProfilingArrests.py
+
+cd ..
 
