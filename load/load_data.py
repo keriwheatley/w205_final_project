4eaf4e2798b52342835d7d690af970d62a6a4fb7
DataDaveH <dhuber@berkeley.edu>
2017-04-17 14:48:23 -0700

    Zillow data support partially added

--- load/load_data.py
+++ load/load_data.py
@@ -8,6 +8,30 @@ import requests
 import datetime
 import json
 import psycopg2
+import pandas as pd
+
+#############################################################################
+# Helper Functions
+#############################################################################
+def isNewer(strDate1, strDate2):
+    """Simple utility function for comparing a particular string date format
+    strDate1 and strDate2 are date strings in 'YYYY-MM' format
+    returns True if strDate2 is newer than strDate1, False otherwise
+    NOTE: interprets empty string as "Beginning of time" meaning:
+        if strDate1 is empty, returns True
+        if strDate1 is not empty but strDate2 IS empty, return False"""
+    if len(strDate1) == 0:
+        return True
+    elif len(strDate2) == 0:
+        return False
+    else:
+        return (int(strDate2[0:4]) > int(strDate1[0:4]) or \
+               (int(strDate2[0:4]) == int(strDate1[0:4]) and int(strDate2[-2:]) > int(strDate1[-2:])))
+
+
+#############################################################################
+# Data Load Functions
+#############################################################################
 
 def load_data_SODA( dict_db_connect, url, table_name, 
                     truncate_table = False, last_update_field = "", last_update_value =  ""):
@@ -114,3 +138,87 @@ def load_data_SODA( dict_db_connect, url, table_name,
     else:
         return True
             
+
+# You will need to implement truncate_table as well
+# Basically, if that value is true, you need to 
+def load_data_Zillow( dict_db_connect, url, table_name, 
+                      truncate_table = False, last_update_value =  ""):
+    """return True if no errors and no new data to load
+    return the newest date string that was loaded into the DB to be stored for next time
+    return False if errors occurred"""
+    try:
+        #Read the CSV from the url directly into a pandas dataframe
+        df = pd.read_csv(url)
+        
+        # PUT THIS WHEREVER IT SHOULD LOGICALLY GO
+        #    - replace newDate with the actual date extracted from the csv file
+        #    - last_update_value will contain what this function returned last time it updated the DB
+        # check if there is newer data
+        if not isNewer( last_update_value, newDate):
+            return True
+        
+        #Drop all rows that aren't for Austin
+        df = df.drop(df[df.City != "Austin"].index)
+        
+        #Drop all rows that aren't for Texas
+        df = df.drop(df[df.State != "TX"].index)
+        
+        #Get the Last Columns
+        last_col_index = len(df.columns)-1
+        
+        #List of columns to drop. 
+        #First 6 cols are regionid (ZIP), region name, city, metro area and county
+        drop_cols = list(range(6,last_col_index))
+        
+        #Drop all columns after the 6th until the last
+        df.drop(df.columns[drop_cols], axis = 1, inplace = True)
+        
+        #Get the date of the latest data
+        date = list(df.columns.values)[6]
+        
+        #Add new columns with date values
+        df['date'] = date
+        
+        # REMOVE? I DON'T SEE ANY NEED TO STORE FILES LOCALLY ONCE THEY ARE IN THE DB
+        #Save to .csv
+        #df.to_csv('updated_%s' % csv, header = False)
+
+        # Connect to database
+        conn = psycopg2.connect( database = dict_db_connect["database"], 
+                                 user = dict_db_connect["user"],
+                                 password = dict_db_connect["password"],
+                                 host = dict_db_connect["host"],
+                                 port = dict_db_connect["port"])
+        cur = conn.cursor()
+        
+        # this is where the data needs to be added to the DB
+        # assume that the table is created before the initial load in a bash script
+        #count = 0
+        for index, row in df.iterrows():
+            # for testing
+            #if count > 10:
+            #    break
+            
+            #
+            #insert values into DB
+            #
+            # for testing, let's print a couple of column headers and some row values
+            print(list(df)[0], list(df)[6])
+            print(str(row['RegionID']), str(row[date]))
+            print("\n")
+        
+            #count += 1
+        
+        #print(df)
+        return date
+    
+    except Exception as e:
+        print("There was an exception while updating the Zillow data:")
+        print(e.args)
+        print(e)
+        return False
+
+
+
+
+
