20bdc5a08f11dd139d29dfd87afecd383fe972df
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-17 14:52:41 -0500

    Update transform_RacialProfilingCitations.py

--- etl_files/transform_RacialProfilingCitations.py
+++ etl_files/transform_RacialProfilingCitations.py
@@ -9,15 +9,15 @@ def data_extract():
         
         # Start runtime
         start_time = datetime.datetime.now()
-        print "Starting data aggregation into (racial_profiling_citations_aggregate) for data source (racial_profiling_citations) at time (" + str(start_time) + ")."
+        print "Starting data transformation into (racial_profiling_citations_temp) for data source (racial_profiling_citations) at time (" + str(start_time) + ")."
         
         # Connect to database
         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
         cur = conn.cursor()
 
         # Empty data table
-        cur.execute("TRUNCATE TABLE racial_profiling_citations_aggregate;");
-        print "Truncated aggregate table."
+        cur.execute("TRUNCATE TABLE racial_profiling_citations_temp;");
+        print "Truncated (racial_profiling_citations_temp) table."
 
         cur.execute("SELECT COALESCE(vl_street_name,'NONE') AS vl_street_name\
             , TO_CHAR(TO_DATE(off_from_date, 'YYYY-MM-DD'),'YYYYMMDD') AS date_number\
@@ -26,7 +26,7 @@ def data_extract():
             , COALESCE(reason_for_stop,'NONE') AS reason_for_stop\
             , COALESCE(msearch_type,'NONE') AS msearch_type\
             , COALESCE(msearch_found,'NONE') AS msearch_found\
-            FROM racial_profiling_citations LIMIT 5;")        
+            FROM racial_profiling_citations;")        
         data = cur.fetchall()
         
         api_key = 'AIzaSyAEiOrh_qZFJBTzEVRKLKYQ3cYFBAvcScs'
@@ -55,32 +55,11 @@ def data_extract():
             print sql
             cur.execute(sql)              
     
-#         "SELECT 
-#             rep_date AS date_number
-#             , zip_code AS zip_code
-#             , sex AS gender
-#             , age_at_offense AS age_at_offense
-#             , apd_race_desc AS race
-#             , reason_for_stop, AS reason_for_stop
-#         FROM 
-#             "
-        
-#         # Write each row to data table
-#         for row in data:
-#             values = ""
-#             columns = ""
-#             for i in row:
-#                 columns += str(i) + ","                
-#                 values += "'" + str(row[i]).replace("'","") + "',"
-#             columns = columns[:-1]
-#             values = values[:-1]
-#             cur.execute("INSERT INTO residential_water_consumption (" + columns + ") VALUES (" + values + ");");
-
-#         # Commit changes to table and close connection
-#         conn.commit()
-#         conn.close()
-#         print "Loaded " + str(len(data)) + " records to data source (residential_water_consumption)."
-#         print "Ended data extract for data source (residential_water_consumption) at time (" + str(datetime.datetime.now()) + ")."
+        # Commit changes to table and close connection
+        conn.commit()
+        conn.close()
+        print "Loaded " + str(len(data)) + " records to data source (racial_profiling_citations_temp)."
+        print "Ended data extract for data source (racial_profiling_citations_temp) at time (" + str(datetime.datetime.now()) + ")."
     
     # Error logging
     except Exception as inst:
