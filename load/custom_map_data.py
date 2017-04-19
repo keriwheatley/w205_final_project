35a46e51d8f2e7670fb5863afd3a2aa7e1724b1d
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 17:25:36 -0500

    Update custom_map_data.py

--- load/custom_map_data.py
+++ load/custom_map_data.py
@@ -53,9 +53,9 @@ def zip_code_map_SODA( dict_db_connect, source_table, target_table, custom_map_c
         print("Dropping temp table if exists.")
         cur.execute("DROP TABLE IF EXISTS " + target_table + ";")
         
-        sql = "CREATE TABLE " + target_table + " AS"
-        sql += " (SELECT *, COALESCE(zip_code,99999) AS zip_code FROM " + source_table
-        sql += " LEFT JOIN zip_code_map ON location = '" + custom_map_col + "');"                
+        sql = "CREATE TABLE " + target_table + " AS (SELECT " + source_table
+        sql += ".*, COALESCE(zip_code_map.zip_code,99999) AS zip_code FROM " + source_table
+        sql += " LEFT JOIN zip_code_map ON zip_code_map.location = '" + source_table + "." + custom_map_col + "');"                
 
         print sql
         cur.execute(sql)
