2a2e1b57642c9c6f5e8432de236fe708d0d1e817
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 16:12:35 -0500

    Update extract_data.py

--- load/extract_data.py
+++ load/extract_data.py
@@ -87,6 +87,8 @@ def extract_data_SODA( dict_db_connect, url, table_name,
             # get latest values
             # NOTE: last_update_value must be wrapped in quotes or it is seen as an int
             request += "&$where=" + last_update_field + ">\'" + last_update_value + "'"
+            print "last_update_field = " + last_update_field
+            print "last_update_value = " + str(last_update_value)
         if truncate_table:
             # if we don't have last update info, or we aren't doing incremental updates,
             # truncate the table before insertion
