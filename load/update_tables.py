df3cb8d9ee67d551dc304922b59587487d975173
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 16:26:27 -0500

    Update update_tables.py

--- load/update_tables.py
+++ load/update_tables.py
@@ -24,8 +24,9 @@ import aggregates as aggs
 
 # config file strings
 CONFIG_FILE_NAME = 'config.ini'
-DATASOURCE_SECTION = "DATASOURCE"
-AGGREGATE_SECTION = "AGGREGATE"
+DATASOURCE_SECTION = 'DATASOURCE'
+AGGREGATE_SECTION = 'AGGREGATE'
+CUSTOM_SECTION = 'CUSTOM'
 URL_KEY = 'url'
 TABLE_NAME_KEY= 'table_name'
 LAST_UPDATE_COL_KEY = 'last_update_col'
@@ -145,7 +146,7 @@ for i in range(2):
 
 print ""
 
-# do we want to run aggregations even if there were errors?
+# Do not run aggregations if error occurred during extract
 if no_errors:
     
     # run aggregations
@@ -214,5 +215,72 @@ if no_errors:
     except Exception as e:
             print(e)
 
+# Do not run custom if errors occurred previously
+if no_errors:
+    
+    # run custom
+    try:
+        # parse CUSTOM section of config file and call each function            
+        for i in range(2):
+            for s in config.sections():
+                if s.startswith(CUSTOM_SECTION):
+                                        
+                    if SOURCE_TABLE_KEY in config.options(s):
+                        source_table = config.get(s, SOURCE_TABLE_KEY)
+                    else:
+                        source_table = ""
 
+                    if TARGET_TABLE_KEY in config.options(s):
+                        target_table = config.get(s, TARGET_TABLE_KEY)
+                    else:
+                        target_table = ""
+                        
+                    if LAST_UPDATE_COL_KEY in config.options(s):
+                        last_update_col_value = config.get(s, LAST_UPDATE_COL_KEY)
+                    else:
+                        last_update_col_value = ""
+
+                    if LAST_UPDATE_VAL_KEY in config.options(s):
+                        last_update_val_value = config.get(s, LAST_UPDATE_VAL_KEY)
+                    else:
+                        last_update_val_value = ""
+
+                    if TYPE_KEY in config.options(s):
+                        type_value = config.get(s, TYPE_KEY)
+                    else:
+                        type_value = ""
+
+                    if TRUNCATE_KEY in config.options(s):
+                        truncate_value = True if config.get(s, TRUNCATE_KEY)[0].lower() == 't' else False
+                    else:
+                        truncate_value = False
+
+                    # at a minimum, we need source table name, target table name, and type
+                    # if any are missing, error out so it can be fixed
+                    if len(source_table) == 0 or len(target_table) == 0 or len(type_value) == 0:
+                        print("Config file error:")            
+                        print("In " + s + "source_table, target_table, and type are required")
+                        config_ok = False            
+
+                    # everything is read in for this source, on verify pass, load the data
+                    if i == 1:
+                        if type_value == "SODA":
+                            ret = custom_function_SODA(dict_db_connect, source_table, target_table, 
+                                                  truncate_value, last_update_col_value, last_update_val_value)
+
+                            #check ret - it will be either a boolean or a string
+                            # here is where we'd write back to the config file if we are not truncating
+                            # and storing the last_update_val
+                            if isinstance(ret, bool):
+                                if ret == False:
+                                    no_errors = False
+                            else:
+                                config.set(s, LAST_UPDATE_VAL_KEY, ret)
+                                with open(CONFIG_FILE_NAME, 'w') as configfile:
+                                    config.write(configfile)                        
+                         
+                        print ""
+
+    except Exception as e:
+            print(e)
 
