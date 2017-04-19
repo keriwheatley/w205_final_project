ab38a1b57c05585b08a1c915ed7d9fcd6b7fcae7
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 14:06:55 -0500

    Update aggregate_data.py

--- load/aggregate_data.py
+++ load/aggregate_data.py
@@ -92,10 +92,10 @@ def aggregate_data_SODA( dict_db_connect, source_table, target_table, truncate_t
         print "Insert completed with status message: " + cur.statusmessage
         conn.commit()
 
-        print "SELECT MAX("+last_update_field+") FROM " + source_table + ";"
-        cur.execute("SELECT MAX("+last_update_field+") FROM " + source_table + ";")
-        last_update_value = cur.fetchone()
-        print last_update_value[0]
+        if not truncate_table and len(last_update_field) > 0:
+            print "SELECT MAX("+last_update_field+") FROM " + source_table + ";"
+            cur.execute("SELECT MAX("+last_update_field+") FROM " + source_table + ";")
+            last_update_value = cur.fetchone()
         
         conn.close()
             
