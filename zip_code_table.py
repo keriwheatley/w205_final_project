4354f4baa22d76178958def3580addb01f761f96
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 14:14:43 -0500

    Update zip_code_table.py

--- zip_code_table.py
+++ zip_code_table.py
@@ -40,7 +40,7 @@ def data_extract():
         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
         cur = conn.cursor()
 
-        sql = "SELECT location, row_number FROM zip_codes WHERE row_number BETWEEN 33384 and 35884;"
+        sql = "SELECT location, row_number FROM zip_codes WHERE row_number BETWEEN 35884 and 38384;"
         
         cur.execute(sql)        
         data = cur.fetchall()
