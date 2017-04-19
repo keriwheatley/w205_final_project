c166ea0f5c36ce3b57e2f96d1b0c58036257a6ef
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 11:37:55 -0500

    Update update_tables.py

--- load/update_tables.py
+++ load/update_tables.py
@@ -24,14 +24,15 @@ import aggregates as aggs
 # config file strings
 CONFIG_FILE_NAME = 'config.ini'
 DATASOURCE_SECTION = "DATASOURCE"
-AGGREGATES_SECTION = "AGGREGATE"
+AGGREGATE_SECTION = "AGGREGATE"
 URL_KEY = 'url'
 TABLE_NAME_KEY= 'table_name'
 LAST_UPDATE_COL_KEY = 'last_update_col'
 LAST_UPDATE_VAL_KEY = 'last_update_val'
 TYPE_KEY = 'type'
 TRUNCATE_KEY = 'truncate'
-
+SOURCE_TABLE_KEY = 'source_table'
+TARGET_TABLE_KEY = 'target_table'
 
 config = cp.ConfigParser()
 
@@ -143,22 +144,68 @@ if no_errors:
     # run aggregations
     try:
         # make sure the config file contains aggregates
-        if not config.has_section( AGGREGATES_SECTION):
-            sys.exit("Error: No sections named 'AGGREGATES' in config file")
-    
-        # parse AGGREGATES section of config file and call each function
-        for function in config.options( AGGREGATES_SECTION):
-            #read any parameters to pass to the function
-            opts = config.get( AGGREGATES_SECTION, function)
-    
-            # call the function        
-            print("Calling: " + function)
-            if len(opts) == 0: 
-                result = getattr(aggs, function)()
-            else:
-                params = [p.strip('"\' ') for p in opts.strip('"\'').split(',')]
-                print("With parameter list: " + str(params))
-                result = getattr(aggs, function)(params)
+        if not config.has_section(AGGREGATE_SECTION):
+            sys.exit("Error: No sections with type 'AGGREGATE' in config file")
+
+        # parse AGGREGATES section of config file and call each function            
+        for i in range(2):
+            for s in config.sections():
+                if s.startswith(AGGREGATE_SECTION):
+
+                    if SOURCE_TABLE_KEY in config.options(s):
+                        source_table = config.get(s, SOURCE_TABLE_KEY)
+                    else:
+                        source_table = ""
+
+
+                    if TARGET_TABLE_KEY in config.options(s):
+                        target_table = config.get(s, TARGET_TABLE_KEY)
+                    else:
+                        taget_table = ""
+                        
+                    if LAST_UPDATE_COL_KEY in config.options(s):
+                        last_update_col_value = config.get(s, LAST_UPDATE_COL_KEY)
+                    else:
+                        last_update_col_value = ""
+
+                    if LAST_UPDATE_VAL_KEY in config.options(s):
+                        last_update_val_value = config.get(s, LAST_UPDATE_VAL_KEY)
+                    else:
+                        last_update_val_value = ""
+
+                    if TYPE_KEY in config.options(s):
+                        type_value = config.get(s, TYPE_KEY)
+                    else:
+                        type_value = ""
+
+                    if TRUNCATE_KEY in config.options(s):
+                        truncate_value = True if config.get(s, TRUNCATE_KEY)[0].lower() == 't' else False
+                    else:
+                        truncate_value = False
+
+            # at a minimum, we need source table name, target table name, and type
+            # if any are missing, error out so it can be fixed
+            if len(source_table) == 0 or len(taget_table) == 0 or len(type_value) == 0:
+                print("Config file error:")            
+                print("In " + s + "source_table, taget_table, and type are required")
+                config_ok = False            
+                
+            # everything is read in for this source, on verify pass, load the data
+            if i == 1:
+                if type_value == "SODA":
+                    ret = aggregate_data_SODA(dict_db_connect, source_table, target_table, 
+                                          truncate_value, last_update_col_value, last_update_val_value)
+                    
+                    #check ret - it will be either a boolean or a string
+                    # here is where we'd write back to the config file if we are not truncating
+                    # and storing the last_update_val
+                    if isinstance(ret, bool):
+                        if ret == False:
+                            no_errors = False
+                    else:
+                        config.set(s, LAST_UPDATE_VAL_KEY, ret)
+                        with open(CONFIG_FILE_NAME, 'w') as configfile:
+                            config.write(configfile)                        
     
             # do something with the result?
     except Exception as e:
