bc73fd1ce4eacf4314be4604092c21e0ae9fb4bb
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 18:37:49 -0500

    Update load_zip_code_map.py

--- load_zip_code_map.py
+++ load_zip_code_map.py
@@ -40,7 +40,7 @@ def data_extract():
         conn = psycopg2.connect(database="finalproject",user="postgres",password="pass",host="localhost",port="5432")
         cur = conn.cursor()
 
-        sql = "SELECT location, row_number FROM zip_codes WHERE row_number BETWEEN 40868 and 43368;"
+        sql = "SELECT location, row_number FROM zip_codes WHERE row_number BETWEEN 43368 and 45868;"
         
         cur.execute(sql)        
         data = cur.fetchall()
