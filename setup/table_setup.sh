57d1189164e0de2fddec1b9cc9e432b81bd47012
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-19 18:45:27 -0500

    Update table_setup.sh

--- setup/table_setup.sh
+++ setup/table_setup.sh
@@ -8,23 +8,23 @@ target_field TEXT, group_by INT, sum_of INT, count_of INT, min_of INT, max_of IN
 psql -U postgres -d finalproject -c "\copy transform_map FROM '~/w205_final_project/transform_map.csv' 
 WITH CSV HEADER DELIMITER AS E'\t'"
 
-# Issued Construction Permits
-# https://data.austintexas.gov/Permitting/Issued-Construction-Permits/3syk-w9eu
-psql -U postgres -d finalproject -c "DROP TABLE IF EXISTS construction_permits;"
-psql -U postgres -d finalproject -c "CREATE TABLE construction_permits (permittype TEXT,permit_type_desc TEXT,
-permit_number TEXT,permit_class_mapped TEXT,permit_class TEXT,work_class TEXT,condominium TEXT,permit_location TEXT,
-description TEXT,tcad_id TEXT,legal_description TEXT,applieddate TEXT,issue_date TEXT,day_issued TEXT,
-calendar_year_issued TEXT,fiscal_year_issued TEXT,issued_in_last_30_days TEXT,issue_method TEXT,status_current TEXT,
-statusdate TEXT,expiresdate TEXT,completed_date TEXT,total_existing_bldg_sqft TEXT,remodel_repair_sqft TEXT,
-total_new_add_sqft TEXT,total_valuation_remodel TEXT,total_job_valuation TEXT,number_of_floors TEXT,housing_units TEXT,
-building_valuation TEXT,building_valuation_remodel TEXT,electrical_valuation TEXT,electrical_valuation_remodel TEXT,
-mechanical_valuation TEXT,mechanical_valuation_remodel TEXT,plumbing_valuation TEXT,plumbing_valuation_remodel TEXT,
-medgas_valuation TEXT,medgas_valuation_remodel TEXT,original_address1 TEXT,original_city TEXT,original_state TEXT,
-original_zip TEXT,council_district TEXT,jurisdiction TEXT,link TEXT,project_id TEXT,masterpermitnum TEXT,
-latitude TEXT,longitude TEXT,location TEXT,contractor_trade TEXT,contractor_company_name TEXT,contractor_full_name TEXT,
-contractor_phone TEXT,contractor_address1 TEXT,contractor_address2 TEXT,contractor_city TEXT,contractor_zip TEXT,
-applicant_full_name TEXT,applicant_org TEXT,applicant_phone TEXT,applicant_address1 TEXT,applicant_address2 TEXT,
-applicant_city TEXT,applicantzip TEXT);"
+# # Issued Construction Permits
+# # https://data.austintexas.gov/Permitting/Issued-Construction-Permits/3syk-w9eu
+# psql -U postgres -d finalproject -c "DROP TABLE IF EXISTS construction_permits;"
+# psql -U postgres -d finalproject -c "CREATE TABLE construction_permits (permittype TEXT,permit_type_desc TEXT,
+# permit_number TEXT,permit_class_mapped TEXT,permit_class TEXT,work_class TEXT,condominium TEXT,permit_location TEXT,
+# description TEXT,tcad_id TEXT,legal_description TEXT,applieddate TEXT,issue_date TEXT,day_issued TEXT,
+# calendar_year_issued TEXT,fiscal_year_issued TEXT,issued_in_last_30_days TEXT,issue_method TEXT,status_current TEXT,
+# statusdate TEXT,expiresdate TEXT,completed_date TEXT,total_existing_bldg_sqft TEXT,remodel_repair_sqft TEXT,
+# total_new_add_sqft TEXT,total_valuation_remodel TEXT,total_job_valuation TEXT,number_of_floors TEXT,housing_units TEXT,
+# building_valuation TEXT,building_valuation_remodel TEXT,electrical_valuation TEXT,electrical_valuation_remodel TEXT,
+# mechanical_valuation TEXT,mechanical_valuation_remodel TEXT,plumbing_valuation TEXT,plumbing_valuation_remodel TEXT,
+# medgas_valuation TEXT,medgas_valuation_remodel TEXT,original_address1 TEXT,original_city TEXT,original_state TEXT,
+# original_zip TEXT,council_district TEXT,jurisdiction TEXT,link TEXT,project_id TEXT,masterpermitnum TEXT,
+# latitude TEXT,longitude TEXT,location TEXT,contractor_trade TEXT,contractor_company_name TEXT,contractor_full_name TEXT,
+# contractor_phone TEXT,contractor_address1 TEXT,contractor_address2 TEXT,contractor_city TEXT,contractor_zip TEXT,
+# applicant_full_name TEXT,applicant_org TEXT,applicant_phone TEXT,applicant_address1 TEXT,applicant_address2 TEXT,
+# applicant_city TEXT,applicantzip TEXT);"
 
 psql -U postgres -d finalproject -c "DROP TABLE IF EXISTS construction_permits_aggregate;"
 psql -U postgres -d finalproject -c "CREATE TABLE construction_permits_aggregate (date_number INT, zip_code INT,
