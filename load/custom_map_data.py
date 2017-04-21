a027720005b9dd909fe0ab726ad19ebc73edfa87
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-21 12:00:17 -0500

    Update custom_map_data.py

--- load/custom_map_data.py
+++ load/custom_map_data.py
@@ -57,6 +57,7 @@ def zip_code_map_SODA( dict_db_connect, source_table, target_table, custom_map_c
         sql += ".*, COALESCE(zip_code_map.zip_code,99999) AS zip_code FROM " + source_table
         sql += " LEFT JOIN zip_code_map ON zip_code_map.location = '" + source_table + "." + custom_map_col + "');"                
 
+        print sql
         cur.execute(sql)
         conn.commit()
         conn.close()
