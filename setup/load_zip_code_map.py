7965463d4e407b95fbaa1b85bfdb3e096035ef26
Keri Wheatley <keriwheatley@users.noreply.github.com>
2017-04-21 07:08:21 -0500

    Update load_zip_code_map.py

--- setup/load_zip_code_map.py
+++ setup/load_zip_code_map.py
@@ -73,12 +73,11 @@ def data_extract():
                 for i in xrange(len(geocode_result[0]['address_components'])):
                     if geocode_result[0]['address_components'][i]['types'][0] == 'postal_code':
                         zip_code = geocode_result[0]['address_components'][i]['long_name']
-
-                        str(row[i].encode('ascii','ignore').replace("'","").replace("\\","")) + "',"
-            sql = "UPDATE zip_code_map SET zip_code = "+zip_code+" WHERE row_number = "+str(row_number)+";"
-            print sql
-            cur.execute(sql)              
-            conn.commit()
+            if len(zip_code) == 5:
+                sql = "UPDATE zip_code_map SET zip_code = "+zip_code+" WHERE row_number = "+str(row_number)+";"
+                print sql
+                cur.execute(sql)              
+                conn.commit()
     
         # Commit changes to table and close connection
         conn.close()
